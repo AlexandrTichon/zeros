8913653b494c45bb6dee6764961794cf43401b3c
Alexand Tichon <38797690+AlexandrTichon@users.noreply.github.com>
2018-09-29 20:30:20 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var zero_number = 0,
+      slag = 0,
+      denominator = 5,
+      power = 1;
+  while (true) {
+      denominator = Math.pow(5, power);
+      slag = Math.floor(number / denominator); 
+      if (slag < 1) break;
+      else zero_number += slag;
+      power++;
+  }
+  return zero_number;
 }
